3acf7930ae16e46b4014e9290d12501973190baf
GuyMishan <gaiu01999@gmail.com>
2021-11-29 08:45:15 +0200

    Updates

---
+++ precache-manifest.1b7212425ce7c8f1d5f4125f62a02355.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18151828e1120fab333cd7cd8b6ecccd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6e28451775664613d727",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "853ef45e7b82bfe8c5d9",
+    "url": "./static/css/main.a6fce8e6.chunk.css"
+  },
+  {
+    "revision": "6e28451775664613d727",
+    "url": "./static/js/2.cc3d0dca.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.cc3d0dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "853ef45e7b82bfe8c5d9",
+    "url": "./static/js/main.fe9c05b0.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  },
+  {
+    "revision": "aa769ff3a68406fab4325027694598af",
+    "url": "./static/media/ap1.aa769ff3.jpg"
+  },
+  {
+    "revision": "be5f4e100c1d07731f928496cf335568",
+    "url": "./static/media/ap2.be5f4e10.jpg"
+  },
+  {
+    "revision": "3d1bf2b37cc06486e555d12abf3cb243",
+    "url": "./static/media/bp1.3d1bf2b3.jpg"
+  },
+  {
+    "revision": "99b3ce933c8ee566c9f8c138e8223c74",
+    "url": "./static/media/bp2.99b3ce93.jpg"
+  },
+  {
+    "revision": "e37baa839dc41d275a22ebd8296fb8e2",
+    "url": "./static/media/cp1.e37baa83.jpg"
+  },
+  {
+    "revision": "e37baa839dc41d275a22ebd8296fb8e2",
+    "url": "./static/media/cp2.e37baa83.jpg"
+  },
+  {
+    "revision": "49e16195d1020a0e0040ec549ddddf13",
+    "url": "./static/media/myProfile.49e16195.jpg"
+  }
+]);
